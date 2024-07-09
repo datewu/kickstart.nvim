2e9f6eef4a1b50ea399ba6dc792c7da2a9908e70
datewu <hihahajun@gmail.com>
2024-07-09 18:10:01 +0800

    set language en_US.utf8

--- init.lua
+++ init.lua
@@ -83,6 +83,8 @@ I hope you enjoy your Neovim journey,
 
 P.S. You can delete this when you're done too. It's your config now! :)
 --]]
+-- set language to english
+vim.cmd 'language en_US.utf8'
 
 -- Set <space> as the leader key
 -- See `:help mapleader`
