7151032f04dcff9c92c7e98d4b79277b190b8b27
datewu <4320960+datewu@users.noreply.github.com>
2025-10-12 20:19:23 +0800

    bye sg

--- lua/custom/plugins/sg.lua
+++ lua/custom/plugins/sg.lua
@@ -1,22 +1,23 @@
+return {}
 -- Use your favorite package manager to install, for example in lazy.nvim
 --  Optionally, you can also install nvim-telescope/telescope.nvim to use some search functionality.
-return {
-  'sourcegraph/sg.nvim',
-  dependencies = {
-    'nvim-lua/plenary.nvim', --[[ "nvim-telescope/telescope.nvim ]]
-  },
-  lazy = false, --
-  -- opts = {
-  --   chat = {
-  --     default_model = 'anthropic/claude-3-5-sonnet-20241022',
-  --   },
-  -- },
-  opts = {
-    chat = {
-      default_model = 'anthropic/claude-3-7-sonnet-20250219',
-    },
-  },
-
-  -- If you have a recent version of lazy.nvim, you don't need to add this!
-  --  build = 'nvim -l build/init.lua',
-}
+-- return {
+--   'sourcegraph/sg.nvim',
+--   dependencies = {
+--     'nvim-lua/plenary.nvim', --[[ "nvim-telescope/telescope.nvim ]]
+--   },
+--   lazy = false, --
+--   -- opts = {
+--   --   chat = {
+--   --     default_model = 'anthropic/claude-3-5-sonnet-20241022',
+--   --   },
+--   -- },
+--   opts = {
+--     chat = {
+--       default_model = 'anthropic/claude-3-7-sonnet-20250219',
+--     },
+--   },
+--
+--   -- If you have a recent version of lazy.nvim, you don't need to add this!
+--   --  build = 'nvim -l build/init.lua',
+-- }
